4f3443b98893fd03c7090597dd137096b1c74a1d
rey hermanses <rey.undot@gmail.com>
2025-10-29 11:56:34 +0700

    add button retake di page take picture

--- src/components/PhotoUploadPage.tsx
+++ src/components/PhotoUploadPage.tsx
@@ -682,9 +682,10 @@ export function PhotoUploadPage({ values, onChange, onBack, onNext }: Props) {
               className="w-8 h-8 sm:w-12 sm:h-12 flex items-center justify-center"
               title="Retake Photo"
             >
-              <svg className="w-6 h-6 sm:w-8 sm:h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-              </svg>
+              <img 
+                src="/camera-retake.png" 
+                alt="Retake" 
+              />
             </button>
 
             {/* Shutter */}
